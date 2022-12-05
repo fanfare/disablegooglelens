b5262d4ce6890a7419abc46108a0ca8b49b64f2a
Rene Ae <fanfare@jollo.org>
2022-12-05 13:26:27 -0600

    0.0.3

--- background.js
+++ background.js
@@ -1,21 +1,27 @@
-chrome.contextMenus.create({
-  id: "disablegooglelens",
-  title: "Search Google for image", 
-  contexts:["image"]
-});
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.onClicked.addListener(function(e) {
-    if (e.menuItemId !== "disablegooglelens") {
-      return null
-    }
-    let URL = e.srcUrl
-    let encodedURL = URL.replaceAll("?", '%3F').replaceAll("&", '%26')
-    encodedURL = encodeURIComponent(URL)
-    let crawlRequestURL = `https://lens.google.com/uploadbyurl?url=${encodedURL}&hl=en&re=df&st=${+ new Date()}&ep=gisbubu`
-    chrome.tabs.create({  
-      url: crawlRequestURL
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: "disablegooglelens",
+      title: "Search Google for image", 
+      contexts:["image"]
+    });    
+    chrome.contextMenus.onClicked.addListener(function(e) {
+      try {
+        if (e.menuItemId !== "disablegooglelens") {
+          return null
+        }
+        let URL = e.srcUrl
+        let encodedURL = URL.replaceAll("?", '%3F').replaceAll("&", '%26')
+        encodedURL = encodeURIComponent(URL)
+        let crawlRequestURL = `https://lens.google.com/uploadbyurl?url=${encodedURL}&hl=en&re=df&st=${+ new Date()}&ep=gisbubu`
+        chrome.tabs.create({  
+          url: crawlRequestURL
+        })
+        return null
+      }
+      catch(e) {
+        console.error(e)
+      }
     })
-    return null
   })
 });
